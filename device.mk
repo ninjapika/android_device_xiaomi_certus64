9fdce55f3cdd65955e7da87163ad30b1817c5910
Ninja Pika <76650191+ninjapika@users.noreply.github.com>
2022-03-10 13:52:17 +0530

    certus64: libshims: Shim missing libmtk_vt_service symbol
    Signed-off-by: Ninja Pika <76650191+ninjapika@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_PACKAGES += \
     mtk-ims-telephony
 
+# Symbols
+PRODUCT_PACKAGES += \
+    libshim_vtservice
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
